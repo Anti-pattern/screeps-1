50bf100d3d70ef116a0ed542959db77d143ebe1c
Anti-pattern <defriez1@gmail.com>
2014-12-06 18:53:06 +0000

    Update jobManager.js

--- jobManager.js
+++ jobManager.js
@@ -1,17 +1,15 @@
 var _ = require('lodash');
-
-var creepDo = require('creepDo')();
+var creepDo = require('creepDo') ();
 var means = require('means');
-
 module.exports = function()
-{
-	//declare base object
-	var jobManager = function() {};
-	//-------------------------------------------------------------------------
 
+	{
+//declare base object
+var jobManager = function() {};
+//-------------------------------------------------------------------------
 	jobManager.actionJobs = function ()
 	{
-		for(var i in Game.creeps) 
+		for(var i in Game.creeps)
 		{
 			var creep = Game.creeps[i];
 			if (creep.memory.job == 'mharvest')
@@ -30,37 +28,34 @@ module.exports = function()
 			{
 				creepDo.collect(creep);
 			}
-
-		}	
-	}
-
+		}
+	};
 	jobManager.assignJobs = function ()
 	{
-		for(var i in Game.creeps) 
+		for(var i in Game.creeps)
 		{
-    		if (jobManager.creepHasMeans(Game.creeps[i], "mharvest"))
-    		{
-    			Game.creeps[i].memory.job = "mharvest"
-    		}
-    		if (jobManager.creepHasMeans(Game.creeps[i], "sharvest"))
-    		{
-    			Game.creeps[i].memory.job = "sharvest";	
-    		}
-    		else if (jobManager.creepHasMeans(Game.creeps[i], "attackHostile"))
-    		{
-    			Game.creeps[i].memory.job = "guard";
-    		}
-    		else if (jobManager.creepHasMeans(Game.creeps[i], "collect"))
-    		{
-    			Game.creeps[i].memory.job = "collect"
-    		}
-    		else
-    		{
-    			Game.creeps[i].memory.job = "nothing";
-    		}
+			if (jobManager.creepHasMeans(Game.creeps[i], "mharvest"))
+			{
+				Game.creeps[i].memory.job = "mharvest";
+			}
+			if (jobManager.creepHasMeans(Game.creeps[i], "sharvest"))
+			{
+				Game.creeps[i].memory.job = "sharvest";
+			}
+			else if (jobManager.creepHasMeans(Game.creeps[i], "attackHostile"))
+			{
+				Game.creeps[i].memory.job = "guard";
+			}
+			else if (jobManager.creepHasMeans(Game.creeps[i], "collect"))
+			{
+				Game.creeps[i].memory.job = "collect";
+			}
+			else
+			{
+				Game.creeps[i].memory.job = "nothing";
+			}
 		}
-	}
-
+	};
 	jobManager.creepHasMeans = function (creep, mean)
 	{
 		var creepParts = [];
@@ -68,29 +63,26 @@ module.exports = function()
 		{
 			creepParts[x] = creep.body[x].type;
 		}
-
-		//console.log('mean: ' + means[mean]);
-		//console.log('creep: ' + creepParts);
+//console.log('mean: ' + means[mean]);
+//console.log('creep: ' + creepParts);
 		var result = _.difference(means[mean], creepParts);
-		//console.log('result: ' + result);
+//console.log('result: ' + result);
 		if (result.length)
 			return false;
 		else
 			return true;
-	}
-
+	};
 	jobManager.countUnitWithMeans = function (mean)
 	{
 		var result = 0;
-		for(var i in Game.creeps) 
+		for(var i in Game.creeps)
 		{
 			if (jobManager.creepHasMeans(Game.creeps[i], mean))
-				result++;
+			result++;
 		}
 		return result;
-	}
-
-	//-------------------------------------------------------------------------
-	//return populated object
+	};
+//-------------------------------------------------------------------------
+//return populated object
 	return jobManager;
-}
+};
