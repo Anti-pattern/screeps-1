33b2fd05ff4f28546f10a29077025c4cf2cdc529
Anti-pattern <defriez1@gmail.com>
2014-12-06 14:25:35 +0000

    Update means.js

--- means.js
+++ means.js
@@ -1,9 +1,11 @@
 module.exports =
 {
-	"attack": [Game.ATTACK, Game.MOVE],
+	"attackHostile": [Game.ATTACK, Game.MOVE],
 	"build": [Game.WORK, Game.MOVE],
 	"deliver": [Game.CARRY, Game.MOVE],
-	"harvest": [Game.WORK, Game.CARRY, Game.MOVE],
+	"mharvest": [Game.WORK, Game.CARRY, Game.MOVE],
+	"sharvest": [Game.WORK, Game.MOVE]
+	"collect": [Game.MOVE, Game.CARRY]
 	"heal": [Game.HEAL, Game.MOVE],
 	"turret": [Game.RANGED_ATTACK],
 	"rangedAttack": [Game.RANGED_ATTACK, Game.MOVE],
